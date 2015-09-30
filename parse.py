396d13b6ed30c9b40aed4fa388d8661287112edf
Daniel Boline <ddboline@gmail.com>
2015-09-30 08:35:13 -0400

    ...

---
+++ parse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    Main module
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import security_log_analysis
+from security_log_analysis.cli import run
+
+if __name__ == '__main__':
+    run()
