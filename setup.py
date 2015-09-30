3dd17078384dc2278b0feb0978a071cbca7a3d4f
Daniel Boline <ddboline@gmail.com>
2015-09-30 17:39:37 -0400

    ...

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(
     package_dir={'security_log_analysis': 'security_log_analysis'},
     package_data={'security_log_analysis': ['templates/*.html',]},
     entry_points={'console_scripts':
-        ['security_log_parse = security_log_analysis.cli:run',
+        ['security_log_parse = security_log_analysis.cli:run_parse',
          'security_log_analyze = security_log_analysis.cli:run_analyze']}
 )
