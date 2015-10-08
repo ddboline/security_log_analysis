a9d9feaee440ed6409d090c482ae43945850f5c1
Daniel Boline <ddboline@gmail.com>
2015-10-08 15:02:12 -0400

    ...

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo /opt/conda/bin/conda install --yes requests pandas dateutil matplotlib boto psycopg2 sqlalchemy nose
+sudo /opt/conda/bin/conda install -c https://conda.anaconda.org/ddboline --yes requests \
+    pandas dateutil matplotlib boto psycopg2 sqlalchemy nose coverage
