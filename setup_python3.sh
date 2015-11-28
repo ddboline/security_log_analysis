79ce6d5d6bf9dc74463ed2698585302755f92c32
Daniel Boline <ddboline@gmail.com>
2015-11-28 12:39:24 -0500

    ...

--- setup_python3.sh
+++ setup_python3.sh
@@ -5,7 +5,7 @@ export LANG="C.UTF-8"
 
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
-sudo apt-get install -y python3-pandas python3-dateutil python3-usb \
+sudo apt-get install -y python3-pandas python3-dateutil python3-pyusb \
                         python3-psycopg2 python3-sqlalchemy python3-nose \
                         python3-coverage python3-numpy python3-pandas whois \
                         python3-pytest python3-setuptools python3-dev
