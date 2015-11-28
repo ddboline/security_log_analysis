7173b580c16b23cf6f7facd553f0ed3fb2312a55
Daniel Boline <ddboline@gmail.com>
2015-11-28 11:04:39 -0500

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 sudo apt-get install -y python-pandas python-dateutil python-usb \
                         python-psycopg2 python-sqlalchemy python-nose \
                         python-coverage python-numpy python-pandas whois \
-                        python-pytest
+                        python-pytest python-setuptools python-dev
