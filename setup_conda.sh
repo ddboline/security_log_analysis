5378d2488a1fd90177c74bd5e5feae3e2ebbd4f0
Daniel Boline <ddboline@gmail.com>
2015-12-12 19:10:38 -0500

    ...

--- setup_conda.sh
+++ setup_conda.sh
@@ -3,4 +3,4 @@
 sudo apt-get update
 sudo apt-get install -y whois
 sudo /opt/conda/bin/conda install -c https://conda.anaconda.org/ddboline --yes requests \
-    pandas dateutil matplotlib boto psycopg2 sqlalchemy nose coverage pytest
+    pandas python-dateutil matplotlib boto psycopg2 sqlalchemy nose coverage pytest
