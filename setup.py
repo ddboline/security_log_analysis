6fcd5083475421f216cfc20d2eef5ac461580905
Daniel Boline <ddboline@gmail.com>
2016-06-17 14:46:18 -0400

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='security_log_analysis',
-    version='0.0.3.4',
+    version='0.0.3.5',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='security_log_analysis',
