3ba9e29c01d1d1c91487382768ae03050ebbca3f
Daniel Boline <ddboline@gmail.com>
2017-03-27 22:16:38 -0400

    ...

--- setup.py
+++ setup.py
@@ -6,12 +6,22 @@ Created on Sun May 17 07:14:20 2015
 @author: ddboline
 """
 from __future__ import (absolute_import, division, print_function)
-
+import sys
 from setuptools import setup
+
+console_scripts = (
+    ('security-log-parse', 'security_log_analysis.cli:run_parse'),
+    ('security-log-analyze', 'security_log_analysis.cli:run_analyze'))
+
+if sys.version_info.major == 2:
+    console_scripts = ['%s = %s' % (x, y) for x, y in console_scripts]
+else:
+    v = sys.version_info.major
+    console_scripts = ['%s%s = %s' % (x, v, y) for x, y in console_scripts]
 
 setup(
     name='security_log_analysis',
-    version='0.0.3.7',
+    version='0.0.3.8',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='security_log_analysis',
@@ -23,8 +33,5 @@ setup(
     package_dir={'security_log_analysis': 'security_log_analysis'},
     package_data={'security_log_analysis': ['templates/*.html']},
     entry_points={
-        'console_scripts': [
-            'security-log-parse = security_log_analysis.cli:run_parse',
-            'security-log-analyze = security_log_analysis.cli:run_analyze'
-        ]
+        'console_scripts': console_scripts
     })
